dbf5fae942b5e913db8c9b1d68b38f495c442fbf
umar-dev-24 <umar.nazar03@gmail.com>
2025-07-28 07:41:59 +0000

    bugs fixed

--- config.py
+++ config.py
@@ -2,3 +2,13 @@
 
 API_KEY = "YOUR_GOOGLE_API_KEY"
 ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+LANGFUSE_BASE_URL = "https://cloud.langfuse.com"
+LANGFUSE_SECRET_KEY = "sk-lf-aaa50ee2-d70b-4bb5-82b1-9abdaa6746d9"
+LANGFUSE_PUBLIC_KEY = "pk-lf-1dec998f-3eb1-437a-b588-4b35f5a89f66"
+from langfuse import Langfuse
+
+langfuse = Langfuse(
+    secret_key="sk-lf-aaa50ee2-d70b-4bb5-82b1-9abdaa6746d9",
+    public_key="pk-lf-1dec998f-3eb1-437a-b588-4b35f5a89f66",
+    host="https://cloud.langfuse.com",
+)
---
+++ test.py
@@ -0,0 +1,6 @@
+# test_import.py
+from langfuse.callback import LangfuseCallbackHandler
+from config import LANGFUSE_BASE_URL, LANGFUSE_SECRET_KEY, LANGFUSE_PUBLIC_KEY
+
+print(LANGFUSE_PUBLIC_KEY)
+print("✅ LangfuseCallbackHandler import successful")
